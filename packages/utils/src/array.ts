0ffb9c08db4d28689140ee2be7c28de5f1491787
Dheeraj Singh Nagdali <dheerajsinghnagdali@gmail.com>
2023-11-24 07:53:43 +0530

    fix: compact return type

--- packages/utils/src/array.ts
+++ packages/utils/src/array.ts
@@ -1,6 +1,10 @@
-export function compact<T>(value: T[]): T extends null | undefined ? never : T {
+export function compact<T>(
+  value: T[],
+): (T extends null | undefined ? never : T)[] {
   const copiedValue = structuredClone(value)
-  return copiedValue.filter(Boolean) as T extends null | undefined ? never : T
+  return copiedValue.filter(Boolean) as (T extends null | undefined
+    ? never
+    : T)[]
 }
 
 export function isArray(value: any): value is any[] {

bbb8af077b3019235ea4b0c1f57f2e81cbc4c1e8
Dheeraj Singh Nagdali <dheerajsinghnagdali@gmail.com>
2023-12-03 19:47:35 +0530

    feat: some DOM utilities

--- packages/dom-utils/src/functions.ts
+++ packages/dom-utils/src/functions.ts
@@ -33,3 +33,93 @@ export function focus<T extends HTMLElement>(element: T) {
 export function blur<T extends HTMLElement>(element: T) {
   return element.blur()
 }
+
+export function addEvent<
+  T extends EventTarget,
+  TListener extends EventListener | EventListenerObject,
+>({
+  target,
+  listener,
+  eventName,
+  options,
+}: {
+  target: T
+  listener: TListener
+  eventName: string
+  options?: boolean | AddEventListenerOptions
+}) {
+  target.addEventListener(eventName, listener, options)
+  const unsubscribe = () =>
+    target.removeEventListener(eventName, listener, options)
+  return unsubscribe
+}
+
+export function contains<T extends Node, K extends Node>(node: T, other: K) {
+  return node.contains(other)
+}
+
+export function compareDocumentPosition<T extends Node, K extends Node>(
+  node: T,
+  other: K,
+) {
+  return node.compareDocumentPosition(other)
+}
+
+export function getOffsetSize<T extends HTMLElement>(element: T) {
+  const { offsetHeight, offsetWidth } = element
+  return {
+    offsetHeight,
+    offsetWidth,
+  }
+}
+
+export function getOffsetDimensions<T extends HTMLElement>(element: T) {
+  const { offsetLeft, offsetTop } = element
+  const { offsetHeight, offsetWidth } = getOffsetSize(element)
+  return {
+    offsetLeft,
+    offsetRight: offsetLeft + offsetWidth,
+    offsetTop,
+    offsetBottom: offsetTop + offsetHeight,
+  }
+}
+
+export function getTabIndex<T extends HTMLElement>(element: T) {
+  return element.tabIndex
+}
+
+export function getStyle<T extends HTMLElement | SVGElement>(element: T) {
+  return element.style
+}
+
+export function isHidden<T extends HTMLElement>(element: T) {
+  return element.hidden
+}
+
+export function getScrolledDimensions() {
+  const scrollX = window.scrollX
+  const scrollY = window.screenY
+
+  return {
+    x: scrollX,
+    y: scrollY,
+  }
+}
+
+export function getScrolledDimensionsCompatible() {
+  const { x, y } = getScrolledDimensions()
+  const scrollTop = x ?? document.documentElement.scrollTop
+  const scrollLeft = y ?? document.documentElement.scrollLeft
+
+  return {
+    x: scrollLeft,
+    y: scrollTop,
+  }
+}
+
+export function enableDesignMode() {
+  document.designMode = "on"
+  return () => {
+    document.designMode = "off"
+  }
+}

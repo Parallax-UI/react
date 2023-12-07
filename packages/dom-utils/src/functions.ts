fc80ea4c7c04afa2ec03d70b7620e0e6727c3a4c
Kirk Flores <113055345+KirSpaceB@users.noreply.github.com>
2023-12-06 19:07:55 -0800

    feat: matchMedia and toPx in dom-utils

--- packages/dom-utils/src/functions.ts
+++ packages/dom-utils/src/functions.ts
@@ -6,6 +6,14 @@ export function getClientRects<T extends Element>(element: T): DOMRectList {
   return element.getClientRects()
 }
 
+export function matchMedia(query: string): MediaQueryList {
+  return window.matchMedia(query)
+}
+
+export function toPx(value: number) {
+  return `${value}px`
+}
+
 export function getParentElement<T extends Element>(element: T) {
   return element.parentElement
 }
